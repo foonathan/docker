f96785ae122aaf3a20adf380327ca9b6ea990c74
Jonathan Müller <git@foonathan.net>
2023-08-30 20:17:54 +0200

    Add GCC 13 and clang 16

--- clang/install-clang.sh
+++ clang/install-clang.sh
@@ -59,6 +59,17 @@ case "${LLVM_VERSION}" in
         exit 1
     fi
     ;;
+16*)
+    MIRROR="github"
+    if [ $(uname -m) = "x86_64" ]; then
+        PLATFORM="x86_64-linux-gnu-ubuntu-22.04"
+    elif [ $(uname -m) = "aarch64" ]; then
+        PLATFORM="aarch64-linux-gnu"
+    else
+        echo "unknown architecture" >/dev/stderr
+        exit 1
+    fi
+    ;;
 esac
 
 case "${MIRROR}" in
